2e87cc8134332c668dda98057c7985ee931e899f
Foz1ljon <faxriddinov0620@gmail.com>
2024-07-07 22:56:40 +0500

    up

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -2,6 +2,7 @@ import { defineStore } from "pinia";
 import api from "axios";
 import { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
+import { useRouter } from "vue-router";
 
 export const useAuthStore = defineStore("auth", {
   state: () => ({
@@ -13,8 +14,8 @@ export const useAuthStore = defineStore("auth", {
         const res = await api.post("https://dummyjson.com/auth/login", form);
         this.token = res.data.token;
         localStorage.setItem("token", res.data.token);
+        useRouter().push({ name: "dashboard" });
         toast.success("Logged in");
-        setTimeout(window.location.reload(), 6000);
       } catch (err) {
         toast.error(err.response?.data?.message || "Login failed");
       } finally {
